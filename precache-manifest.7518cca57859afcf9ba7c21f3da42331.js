e52579a4126413309d43003c6a20a19adf8b6dcb
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-12-29 12:16:58 +0630

    Updates

---
+++ precache-manifest.7518cca57859afcf9ba7c21f3da42331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78dc6f69646c587684078ed349d2bddd",
+    "url": "/React-Query-Simple-Project/index.html"
+  },
+  {
+    "revision": "7920c9fec2436fa3f77a",
+    "url": "/React-Query-Simple-Project/static/css/main.8c1b2c95.chunk.css"
+  },
+  {
+    "revision": "025b3c71f546fe11001e",
+    "url": "/React-Query-Simple-Project/static/js/2.01babcfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Query-Simple-Project/static/js/2.01babcfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7920c9fec2436fa3f77a",
+    "url": "/React-Query-Simple-Project/static/js/main.9e0ba2ac.chunk.js"
+  },
+  {
+    "revision": "1453ef1f9b4a358667d3",
+    "url": "/React-Query-Simple-Project/static/js/runtime-main.6dda29a9.js"
+  }
+]);
